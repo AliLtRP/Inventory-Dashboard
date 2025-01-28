d8c77c02e6bf384fd3b73e26edc19984d1614942
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2025-01-28 21:00:54 +0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'dashboard',
+    pathMatch: "full",
+    loadComponent: () => import("./features/dashboard/dashboard.component").then(m => {return m.DashboardComponent})
+},
+{
+    path: 'inventory',
+    pathMatch: "full",
+    loadComponent: () => import("./features/inventory/inventory.component").then(m => {return m.InventoryComponent})
+}
+];
--- src/app/shared/sider-nav/sider-nav.component.ts
+++ src/app/shared/sider-nav/sider-nav.component.ts
@@ -1,16 +1,19 @@
 import { Component, signal } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sider-nav',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sider-nav.component.html',
   styleUrl: './sider-nav.component.scss'
 })
 export class SiderNavComponent {
   position = signal<number>(1);
 
-  icons = [
-    {id: 1, name:'HomeIcon.svg', title: "Dashobard"}, 
+  constructor(private router: Router) {}
+
+  icons: Array<{id: number, name: string, title: string}> = [
+    {id: 1, name:'HomeIcon.svg', title: "Dashboard"}, 
     {id: 2, name:'InventoryIcon.svg', title: "Inventory"}, 
     {id: 3, name:'ReportsIcon.svg', title: "Reports"}, 
     {id: 4, name:'SuppliersIcon.svg', title: "Suppliers"}, 
@@ -20,5 +23,7 @@ export class SiderNavComponent {
 
   handlePositionChange(position: number) {
     this.position.set(position);
+    const route = this.icons[position-1].title.toLowerCase();
+    this.router.navigate([route]);
   }
 }
